1a52ef3bbd11d26b50bd534b6bf71320b63c1f02
Jackson Hurley <jacksonwhurley@gmail.com>
2025-10-24 15:45:29 -0400

    highlighter weirdness

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   // Keep Next.js from traversing outside the project when tracing files.
   outputFileTracingRoot: __dirname,
+
+  webpack: (config) => {
+    // Ignore refractor language imports from react-syntax-highlighter
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'refractor/lang': false,
+    };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
